ccafea219315299891f80e13ed53bf0008c934f4
Justin Grant <jgrant27@gmail.com>
2019-01-24 11:11:48 -0800

    Use &str slices ; Break up into functions ; Add back reading from stdin

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,48 @@
 use std::io::{self, BufRead, BufReader};
-use std::collections::{BTreeMap};
+use std::collections::{HashMap};
 use std::fs::File;
-
+use std::iter::FromIterator;
+use std::fmt::Debug;
 
 const NGRAM_LEN: usize = 3;
 const TOP_LIMIT: usize = 100;
+const SPACE: char = ' ';
 
-
-pub fn clean_word(word: &str) -> String {
-    word.chars().filter(|ch| ch.is_alphabetic())
-        .collect::<String>().to_uppercase()
+pub fn load_words<T: Debug, R: Iterator<Item=Result<String, T>>>(lines: R) -> String {
+    let mut nbuf = String::new();
+    lines.for_each(|line| {
+        line.unwrap().split_whitespace().for_each(|word| {
+            let mut cword = word.chars().filter(|ch| ch.is_alphabetic())
+                .collect::<String>().to_uppercase();
+            cword.push(SPACE);
+            if !cword.is_empty() {
+                nbuf.push_str(&cword);
+            }
+        });
+    });
+    dbg!(nbuf.len());
+    nbuf
 }
 
-pub fn get_ngram_counts<'a>(words: &'a Vec<String>, n: usize)
-                            -> Vec<(&'a[String], usize)> {
-    let mut counts: BTreeMap<&[String], usize> = BTreeMap::new();
-    for ngram in words.windows(n) {
-        *counts.entry(ngram).or_insert(0) += 1;
+pub fn get_ngram_counts<'a>(nbuf: &'a String) -> Vec<(&'a str, usize)> {
+    let mut cmap: HashMap<&str, usize> = HashMap::new();
+    let ixs = nbuf.match_indices(SPACE).map(|(i, _)| i).collect::<Vec<usize>>();
+    dbg!(ixs.len());
+    for (n, _) in ixs.iter().enumerate() {
+        let start = ixs[n] + 1;
+        if n + NGRAM_LEN < ixs.len() - 1 {
+            let end = ixs[n + NGRAM_LEN];
+            let ngram = &nbuf[start..end];
+            *cmap.entry(&ngram).or_insert(0) += 1;
+        }
     }
-    use std::iter::FromIterator;
-    let mut sorted = Vec::from_iter(counts);
-    sorted.sort_by(|&(_, c1), &(_, c2)| c2.cmp(&c1));
-    sorted
+    let mut ngram_counts = Vec::from_iter(cmap);
+    ngram_counts.sort_by(|&(s1, _), &(s2, _)| s1.cmp(&s2));
+    ngram_counts.sort_by(|&(_, c1), &(_, c2)| c2.cmp(&c1));
+    ngram_counts
 }
 
-pub fn from_stdin() -> String {
-    let stdin = io::stdin();
-    let words = stdin.lock().lines().flat_map(|line| {
-        line.unwrap().split_whitespace()
-            .map(|word| clean_word(word))
-            .filter(|word| !word.is_empty())
-            .collect::<Vec<String>>()
-    }).collect::<Vec<String>>();
-    let ngram_counts = get_ngram_counts(&words, NGRAM_LEN);
+pub fn get_top_counts<'a>(ngram_counts: &'a Vec<(&'a str, usize)>) -> String {
     let mut res: String = String::new();
     for (ngram, count) in ngram_counts.into_iter().take(TOP_LIMIT) {
         res.push_str(format!("{:?} {}\n", ngram, count).as_str());
@@ -42,16 +52,18 @@ pub fn from_stdin() -> String {
 
 pub fn from_path(path: &str) -> String {
     let file = File::open(path).unwrap();
-    let words = BufReader::new(file).lines().flat_map(|line| {
-        line.unwrap().split_whitespace()
-            .map(|word| clean_word(word))
-            .filter(|word| !word.is_empty())
-            .collect::<Vec<String>>()
-    }).collect::<Vec<String>>();
-    let ngram_counts = get_ngram_counts(&words, NGRAM_LEN);
-    let mut res: String = String::new();
-    for (ngram, count) in ngram_counts.into_iter().take(TOP_LIMIT) {
-        res.push_str(format!("{:?} {}\n", ngram, count).as_str());
-    }
-    res
+    let mut lines = BufReader::new(file).lines();
+
+    let nbuf = load_words(&mut lines);
+    let ngram_counts = get_ngram_counts(&nbuf);
+    get_top_counts(&ngram_counts)
+}
+
+pub fn from_stdin() -> String {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    let nbuf = load_words(&mut lines);
+    let ngram_counts = get_ngram_counts(&nbuf);
+    get_top_counts(&ngram_counts)
 }
--- tests/ngrams_tests.rs
+++ tests/ngrams_tests.rs
@@ -2,19 +2,13 @@
 mod ngram_tests {
 
     use std::fs;
-    use ngrams::{clean_word, from_path};
-
-    #[test]
-    fn clean_word_test() {
-        assert_eq!("NGRAM", clean_word("! ngram. "));
-        assert_eq!("NGRAM", clean_word("n-gram !!!"));
-    }
+    use ngrams::{from_path};
 
     #[test]
     fn from_path_test() {
-        assert_eq!(from_path("./text/pg2009.txt").trim(),
-                   fs::read_to_string("./text/pg2009_res.txt").unwrap().trim());
         assert_eq!(from_path("./text/1661.txt").trim(),
                    fs::read_to_string("./text/1661_res.txt").unwrap().trim());
+        assert_eq!(from_path("./text/pg2009.txt").trim(),
+                   fs::read_to_string("./text/pg2009_res.txt").unwrap().trim());
     }
 }
